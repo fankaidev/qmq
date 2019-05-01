c246f70bcc2d04587c6c6886a23259f24ac34727
fankai <fankai@fklj.net>
2019-05-02 07:21:09 +0800

    lombok

--- qmq-remoting/src/main/java/qunar/tc/qmq/protocol/consumer/AckRequest.java
+++ qmq-remoting/src/main/java/qunar/tc/qmq/protocol/consumer/AckRequest.java
@@ -16,9 +16,12 @@
 
 package qunar.tc.qmq.protocol.consumer;
 
+import lombok.Data;
+
 /**
  * @author yiqun.fan create on 17-8-24.
  */
+@Data
 public class AckRequest {
     private static final byte UNSET = -1;
     private String subject;
@@ -27,46 +30,6 @@ public class AckRequest {
     private long pullOffsetBegin;
     private long pullOffsetLast;
     private byte isBroadcast = UNSET;
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getGroup() {
-        return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
-    }
-
-    public String getConsumerId() {
-        return consumerId;
-    }
-
-    public void setConsumerId(String consumerId) {
-        this.consumerId = consumerId;
-    }
-
-    public long getPullOffsetBegin() {
-        return pullOffsetBegin;
-    }
-
-    public void setPullOffsetBegin(long pullOffsetBegin) {
-        this.pullOffsetBegin = pullOffsetBegin;
-    }
-
-    public long getPullOffsetLast() {
-        return pullOffsetLast;
-    }
-
-    public void setPullOffsetLast(long pullOffsetLast) {
-        this.pullOffsetLast = pullOffsetLast;
-    }
 
     public void setBroadcast(byte isBroadcast) {
         this.isBroadcast = isBroadcast;
