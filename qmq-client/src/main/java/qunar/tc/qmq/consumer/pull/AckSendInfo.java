f754bd8f7e5257fadd773a7b5f94b29e8d08e4bd
fankai <fankai@fklj.net>
2019-05-03 12:56:30 +0800

    lombok

--- qmq-client/src/main/java/qunar/tc/qmq/consumer/pull/AckSendInfo.java
+++ qmq-client/src/main/java/qunar/tc/qmq/consumer/pull/AckSendInfo.java
@@ -16,40 +16,15 @@
 
 package qunar.tc.qmq.consumer.pull;
 
+import lombok.Data;
+
 /**
  * @author yiqun.fan create on 17-8-20.
  */
+@Data
 class AckSendInfo {
     private int toSendNum = 0;
     private long minPullOffset = -1;
     private long maxPullOffset = -1;
 
-    public int getToSendNum() {
-        return toSendNum;
-    }
-
-    public void setToSendNum(int toSendNum) {
-        this.toSendNum = toSendNum;
-    }
-
-    public long getMinPullOffset() {
-        return minPullOffset;
-    }
-
-    public void setMinPullOffset(long minPullOffset) {
-        this.minPullOffset = minPullOffset;
-    }
-
-    public long getMaxPullOffset() {
-        return maxPullOffset;
-    }
-
-    public void setMaxPullOffset(long maxPullOffset) {
-        this.maxPullOffset = maxPullOffset;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + minPullOffset + ", " + maxPullOffset + ", " + toSendNum + ")";
-    }
 }
