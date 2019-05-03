fe1ac5f3dcd88c27123fff47f5e31fb978bc365d
fankai <fankai@fklj.net>
2019-05-03 11:46:36 +0800

    lombok

--- qmq-remoting/src/main/java/qunar/tc/qmq/protocol/consumer/PullRequest.java
+++ qmq-remoting/src/main/java/qunar/tc/qmq/protocol/consumer/PullRequest.java
@@ -16,6 +16,7 @@
 
 package qunar.tc.qmq.protocol.consumer;
 
+import lombok.Data;
 import qunar.tc.qmq.TagType;
 
 import java.util.Collections;
@@ -24,6 +25,7 @@ import java.util.List;
 /**
  * @author yiqun.fan create on 17-7-4.
  */
+@Data
 public class PullRequest {
     private String subject;
     private String group;
@@ -39,109 +41,12 @@ public class PullRequest {
 
     private List<byte[]> tags = Collections.emptyList();
 
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getGroup() {
-        return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
-    }
-
-    public int getRequestNum() {
-        return requestNum;
-    }
-
-    public void setRequestNum(int requestNum) {
-        this.requestNum = requestNum;
-    }
-
-    public long getTimeoutMillis() {
-        return timeoutMillis;
-    }
-
-    public void setTimeoutMillis(long timeoutMillis) {
-        this.timeoutMillis = timeoutMillis;
-    }
-
-    public long getOffset() {
-        return offset;
-    }
-
-    public void setOffset(long offset) {
-        this.offset = offset;
-    }
-
-    public long getPullOffsetBegin() {
-        return pullOffsetBegin;
-    }
-
-    public void setPullOffsetBegin(long pullOffsetBegin) {
-        this.pullOffsetBegin = pullOffsetBegin;
-    }
-
-    public long getPullOffsetLast() {
-        return pullOffsetLast;
-    }
-
-    public void setPullOffsetLast(long pullOffsetLast) {
-        this.pullOffsetLast = pullOffsetLast;
-    }
-
-    public void setConsumerId(String consumerId) {
-        this.consumerId = consumerId;
-    }
-
-    public String getConsumerId() {
-        return consumerId;
-    }
-
     public boolean isBroadcast() {
         return isBroadcast;
     }
 
     public void setBroadcast(boolean broadcast) {
         isBroadcast = broadcast;
-    }
-
-    public int getTagTypeCode() {
-        return tagTypeCode;
-    }
-
-    public void setTagTypeCode(int tagTypeCode) {
-        this.tagTypeCode = tagTypeCode;
-    }
-
-    public List<byte[]> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<byte[]> tags) {
-        this.tags = tags;
-    }
-
-    @Override
-    public String toString() {
-        return "PullRequest{" +
-                "subject='" + subject + '\'' +
-                ", group='" + group + '\'' +
-                ", requestNum=" + requestNum +
-                ", timeoutMillis=" + timeoutMillis +
-                ", offset=" + offset +
-                ", pullOffsetBegin=" + pullOffsetBegin +
-                ", pullOffsetLast=" + pullOffsetLast +
-                ", consumerId='" + consumerId + '\'' +
-                ", isBroadcast=" + isBroadcast +
-                ", tagTypeCode=" + tagTypeCode +
-                ", tags=" + tags +
-                '}';
     }
 
 }
